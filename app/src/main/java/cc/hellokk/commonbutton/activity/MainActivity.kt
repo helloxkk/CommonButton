2c1c310f75dc7ca6a4fa05a546b57c355b4a0ffb
helloxkk <xk6321@gmail.com>
2018-09-05 14:55:25 +0800

    update

---
+++ app/src/main/java/cc/hellokk/commonbutton/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cc.hellokk.commonbutton.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cc.hellokk.commonbutton.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
