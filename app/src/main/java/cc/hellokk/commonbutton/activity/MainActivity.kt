cccebe350d3b3419d65e5878829f0daf2130658d
helloxkk <xk6321@gmail.com>
2018-09-12 14:36:52 +0800

    update

--- app/src/main/java/cc/hellokk/commonbutton/activity/MainActivity.kt
+++ app/src/main/java/cc/hellokk/commonbutton/activity/MainActivity.kt
@@ -2,12 +2,29 @@ package cc.hellokk.commonbutton.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import cc.hellokk.commonbutton.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_1.setOnClickListener { showToast(btn_1.text) }
+        btn_2.setOnClickListener { showToast(btn_2.text) }
+        btn_3.setOnClickListener { showToast(btn_3.text) }
+        btn_4.setOnClickListener { showToast(btn_4.text) }
+        btn_5.setOnClickListener { showToast(btn_5.text) }
+        btn_6.setOnClickListener { showToast(btn_6.text) }
+    }
+
+    private var mToast: Toast? = null
+
+    private fun showToast(text: CharSequence?) {
+        if (mToast == null)
+            mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT)
+        mToast?.setText(text)
+        mToast?.show()
     }
 }
